8764a49dd7417404d34ff682594fe6b6daf54a27
Henil61298 <44196527+Henil61298@users.noreply.github.com>
2024-01-12 22:33:02 +0530

    Update Appframe.java

--- Appframe.java
+++ Appframe.java
@@ -5,33 +5,41 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 class Appframe implements ActionListener{
     JButton compressButton;
     JButton decompressButton;
 
+    JPanel panel;
     JFrame frame;
 
     Appframe(){
+        panel = new JPanel();
+        panel.setBorder(new EmptyBorder(150, 0, 0, 0));
+
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         compressButton = new JButton("Compress");
-        compressButton.setBounds(20, 170, 100, 30);
+//        compressButton.setBounds(0, 0, 100, 30);
         compressButton.addActionListener(this);
-        frame.add(compressButton);
+        panel.add(compressButton);
+        compressButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
         decompressButton = new JButton("Decompress");
-        decompressButton.setBounds(300, 170, 100, 30);
+//        decompressButton.setBounds(300, 170, 100, 30);
         decompressButton.addActionListener(this);
+        panel.add(decompressButton);
+        decompressButton.setBorderPainted(true);
+        decompressButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-        frame.add(decompressButton);
+        panel.setBackground(Color.BLACK);
+
+        frame.add(panel);
+
         frame.setBounds(0, 0, 500, 400);
-        frame.getContentPane().setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         frame.setVisible(true);
     }
 
